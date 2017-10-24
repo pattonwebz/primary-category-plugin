0b6dd02017efe7a4d1503e336a617984ab8feef9
William Patton <will@pattonwebz.com>
2017-10-24 23:31:39 +0100

    A some functions to include scripts on edit pages plus some inline data

--- inc/metaboxes.php
+++ inc/metaboxes.php
@@ -7,3 +7,32 @@
   *
   * @package  Primary Category Plugin
   */
+
+class pwwp_primary_category_metabox_modifications {
+
+	// constructor function to add actions necessary for adding the primary
+	// category functions to the editor screen.
+	public function __construct() {
+		// Output the script in the post edit and new post pages or admin.
+		add_action( 'admin_enqueue_scripts', array( $this, 'output_primary_category_admin_script' ), 10 );
+	}
+
+	public function output_primary_category_admin_script( $hook ) {
+		// get some variables we'll use in the script.#
+		global $post;
+
+		// if on post-new or post pages then enqueue our scripts.
+		if ( 'post-new.php' === $hook || 'post.php' === $hook ) {
+			wp_enqueue_script( 'pwwp-pc-functions', PRIMARY_CATEGORY_PLUGIN_URL . 'js/primary-category-functions.js', array( 'jquery' ) );
+			$post_id = $post->ID;
+			wp_add_inline_script( 'pwwp-pc-functions', '
+//<![CDATA[
+	var pwwp_pc_data;
+	pwwp_pc_data = { post_ID: ' . $post_id . ' };
+//]]>' );
+		}
+
+	}
+}
+
+$pwwp_pc_metabox = new pwwp_primary_category_metabox_modifications;
