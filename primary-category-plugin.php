2cfbd9cba6ee7eb5faf5a172cef5a6c4bb3f82d6
William Patton <will@pattonwebz.com>
2017-10-26 17:19:41 +0100

    keep correct constants

--- primary-category-plugin.php
+++ primary-category-plugin.php
@@ -24,10 +24,6 @@ if ( ! defined( 'PRIMARY_CATEGORY_PLUGIN_URL' ) ) {
 	define( 'PRIMARY_CATEGORY_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
 
-if ( ! defined( 'PRIMARY_CATEGORY_PLUGIN_VERSION' ) ) {
-	define( 'PRIMARY_CATEGORY_PLUGIN_VERSION', '1.0.0' );
-}
-
 /** -------------------------------- *
  * includes
  * ---------------------------------- */
