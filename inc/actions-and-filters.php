7a3022287c9af6837b0caf09504c9039683a8fff
William Patton <will@pattonwebz.com>
2017-10-26 03:56:49 +0100

    Filter the args used when building category lists for our custom widget

--- inc/actions-and-filters.php
+++ inc/actions-and-filters.php
@@ -7,3 +7,17 @@
   *
   * @package  Primary Category Plugin
   */
+
+/**
+ * Filters args used in the primary category custom widget.
+ * @param  array $args array of args used when building category lists.
+ * @return array updates args array with a meta_key value.
+ */
+function pwwp_pc_filter_widget_args( $args ) {
+	$args = array_merge( $args, array(
+		'meta_key'	=> '_pwwp_pc_selected_id',
+	) );
+	return $args;
+}
+add_filter( 'pwwp_widget_primary_categories_dropdown_args', 'pwwp_pc_filter_widget_args' );
+add_filter( 'pwwp_widget_primary_categories_args', 'pwwp_pc_filter_widget_args' );
