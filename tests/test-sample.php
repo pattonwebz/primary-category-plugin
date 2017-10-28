102f21cfd54698bd4accd8465119ecdb822a5d30
William Patton <will@pattonwebz.com>
2017-10-28 21:33:25 +0100

    Added some tests for class existance.

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -21,13 +21,24 @@ class SampleTest extends WP_UnitTestCase {
 	/**
 	 * Test if a constant defined by plugin is available.
 	 */
-	function test_plugin_loaded() {
+	function test_plugin_loaded_const_dir() {
 		$this->assertTrue( defined( 'PRIMARY_CATEGORY_PLUGIN_DIR' ) );
 	}
 	/**
 	 * Test if a constant defined by plugin is available.
 	 */
-	function test_plugin_loaded() {
+	function test_plugin_loaded_const_url() {
 		$this->assertTrue( defined( 'PRIMARY_CATEGORY_PLUGIN_URL' ) );
 	}
+
+	function test_metabox_modifications_class() {
+		$this->assertTrue( class_exists( 'PWWP_Primary_Category_Metabox_Modifications' ) );
+	}
+	function test_widget_class() {
+		$this->assertTrue( class_exists( 'PWWP_Widget_Primary_Categories' ) );
+	}
+	function test_metabox_shortcode_class() {
+		$this->assertTrue( class_exists( 'PWWP_PC_Query_Shortcode' ) );
+	}
+
 }
