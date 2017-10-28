c84ae9499da47285d9c7a874dd392cc86b7abe7a
William Patton <will@pattonwebz.com>
2017-10-28 21:39:59 +0100

    Try again to exclude the test file

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -30,13 +30,21 @@ class SampleTest extends WP_UnitTestCase {
 	function test_plugin_loaded_const_url() {
 		$this->assertTrue( defined( 'PRIMARY_CATEGORY_PLUGIN_URL' ) );
 	}
-
+	/**
+	 * Test if this from our plugin is loaded and exists.
+	 */
 	function test_metabox_modifications_class() {
 		$this->assertTrue( class_exists( 'PWWP_Primary_Category_Metabox_Modifications' ) );
 	}
+	/**
+	 * Test if this from our plugin is loaded and exists.
+	 */
 	function test_widget_class() {
 		$this->assertTrue( class_exists( 'PWWP_Widget_Primary_Categories' ) );
 	}
+	/**
+	 * Test if this from our plugin is loaded and exists.
+	 */
 	function test_metabox_shortcode_class() {
 		$this->assertTrue( class_exists( 'PWWP_PC_Query_Shortcode' ) );
 	}
