0a8c1aa7f5faee54a7277854be34509915fb1e40
William Patton <will@pattonwebz.com>
2017-10-29 21:09:26 +0000

    Assert both constants are defined in a single test

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,25 +1,20 @@
 <?php
 /**
- * Class SampleTest
+ * Class PluginActiveTest
  *
  * @package Primary_Category_Plugin
  */
 
 /**
- * Sample test case.
+ * PluginActiveTest.
  */
-class SampleTest extends WP_UnitTestCase {
+class PluginActiveTest extends WP_UnitTestCase {
 
 	/**
 	 * Test if a constant defined by plugin is available.
 	 */
 	function test_plugin_loaded_const_dir() {
 		$this->assertTrue( defined( 'PRIMARY_CATEGORY_PLUGIN_DIR' ) );
-	}
-	/**
-	 * Test if a constant defined by plugin is available.
-	 */
-	function test_plugin_loaded_const_url() {
 		$this->assertTrue( defined( 'PRIMARY_CATEGORY_PLUGIN_URL' ) );
 	}
 
